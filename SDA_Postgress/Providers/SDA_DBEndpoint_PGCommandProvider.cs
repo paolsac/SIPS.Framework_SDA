33db978e5f9ce074447a0ecea8d616e092b1871a
nh1saccapa <paolo.saccani.nh@pirelli.com>
2024-11-10 23:19:51 +0100

    add datareader and bulk copy

--- SDA_Postgress/Providers/SDA_DBEndpoint_PGCommandProvider.cs
+++ SDA_Postgress/Providers/SDA_DBEndpoint_PGCommandProvider.cs
@@ -4,8 +4,10 @@ using Microsoft.Extensions.Logging;
 using Npgsql;
 using SIPS.Framework.Core.AutoRegister.Interfaces;
 using SIPS.Framework.Core.Providers;
+using SIPS.Framework.SDA.Api;
 using SIPS.Framework.SDA.interfaces;
 using SIPS.Framework.SDA.Providers.Base;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -19,6 +21,7 @@ namespace SIPS.Framework.SDA_Postgress.Providers
         private readonly ILogger<SDA_DBEndpoint_PGCommandProvider> _logger;
         private readonly IConfiguration _configuration;
         private bool disposedValue;
+        private readonly Dictionary<Guid, IDbConnection> _openConnections;
 
         public SDA_DBEndpoint_PGCommandProvider(ILogger<SDA_DBEndpoint_PGCommandProvider> logger,
                                                 IConfiguration configuration,
@@ -27,6 +30,7 @@ namespace SIPS.Framework.SDA_Postgress.Providers
         {
             _logger = logger;
             _configuration = configuration;
+            _openConnections = new Dictionary<Guid, IDbConnection>();
         }
 
         public string ConnectionStringName { get; set; }
@@ -78,6 +82,214 @@ namespace SIPS.Framework.SDA_Postgress.Providers
                     connection.Execute(query, parameter);
                 }
             }
+        }
+
+        public SDA_BullkCopyResult BulkLoadFromDataTable(string tableName, DataTable dtTable, SDA_ColumnMappingCollections columnMappings, SDA_CommandOptions options)
+        {
+            long rowsCopied = 0;
+            DateTime startTime = DateTime.Now;
+            try
+            {
+                string connectionString = GetConnectionString();
+
+                string columns = string.Join(",", dtTable.Columns.Cast<DataColumn>().Select(c => c.ColumnName));
+
+                // if columnMappings is not null, it means that the columns are mapped
+                if (columnMappings != null && columnMappings.GetMappings().Any())
+                {
+                    SDA_ColumnMappingCollections.ColumnMappingType[] supportedMappingTypes =
+                        new SDA_ColumnMappingCollections.ColumnMappingType[] {
+                            SDA_ColumnMappingCollections.ColumnMappingType.nameToName,
+                            SDA_ColumnMappingCollections.ColumnMappingType.indexToName,
+                        };
+                    if (!supportedMappingTypes.Contains(columnMappings.MappingType))
+                    {
+                        throw new System.Exception($"Mapping type {columnMappings.MappingType} not supported");
+                    }
+                    switch (columnMappings.MappingType)
+                    {
+                        case SDA_ColumnMappingCollections.ColumnMappingType.nameToName:
+                            columns = string.Join(",", dtTable.Columns.Cast<DataColumn>().Select(c => columnMappings.GetMappings().First(m => m.SourceColumn == c.ColumnName).DestinationColumn));
+                            break;
+                        case SDA_ColumnMappingCollections.ColumnMappingType.indexToName:
+                            columns = string.Join(",", dtTable.Columns.Cast<DataColumn>().Select(c => columnMappings.GetMappings().First(m => m.SourceColumnIndex == c.Ordinal).DestinationColumn));
+                            break;
+                    }
+                }
+
+                using (var connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (var writer = connection.BeginBinaryImport($"COPY {tableName} ({columns}) FROM STDIN (FORMAT BINARY)"))
+                    {
+                        foreach (DataRow row in dtTable.Rows)
+                        {
+                            writer.StartRow();
+                            foreach (DataColumn col in dtTable.Columns)
+                            {
+                                writer.Write(row[col]);
+                            }
+                        }
+                        writer.Complete();
+                    }
+                }
+                rowsCopied = dtTable.Rows.Count;
+                return new SDA_BullkCopyResult() { Success = true, RowsCopied = rowsCopied, Duration = DateTime.Now - startTime };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error during bulk load from DataTable");
+                return new SDA_BullkCopyResult() { Success = false, ErrorMessage = ex.Message };
+            }
+        }
+
+        public SDA_BullkCopyResult BulkLoadFromDataReader(string tableName, IDataReader dtReader, SDA_ColumnMappingCollections columnMappings, SDA_CommandOptions options)
+        {
+            long rowsCopied = 0;
+            DateTime startTime = DateTime.Now;
+            try
+            {
+                string connectionString = GetConnectionString();
+
+                // stringa  con i nomi delle colonne utilizzando dtreader, elenco concatenato con virgola
+                List<string> columnNames = new List<string>();
+
+                for (int i = 0; i < dtReader.FieldCount; i++)
+                {
+                    columnNames.Add(dtReader.GetName(i));
+                }
+                string columns = string.Join(",", columnNames);
+
+                // if columnMappings is not null, it means that the columns are mapped
+                if (columnMappings != null && columnMappings.GetMappings().Any())
+                {
+                    SDA_ColumnMappingCollections.ColumnMappingType[] supportedMappingTypes =
+                        new SDA_ColumnMappingCollections.ColumnMappingType[] {
+                            SDA_ColumnMappingCollections.ColumnMappingType.nameToName,
+                            SDA_ColumnMappingCollections.ColumnMappingType.indexToName,
+                        };
+                    if (!supportedMappingTypes.Contains(columnMappings.MappingType))
+                    {
+                        throw new System.Exception($"Mapping type {columnMappings.MappingType} not supported");
+                    }
+                    switch (columnMappings.MappingType)
+                    {
+                        case SDA_ColumnMappingCollections.ColumnMappingType.nameToName:
+                            columns = string.Join(",", columnNames.Select(c => columnMappings.GetMappings().First(m => m.SourceColumn == c).DestinationColumn));
+                            break;
+                        case SDA_ColumnMappingCollections.ColumnMappingType.indexToName:
+                            columns = string.Join(",", columnNames.Select(c => columnMappings.GetMappings().First(m => m.SourceColumnIndex == columnNames.IndexOf(c)).DestinationColumn));
+                            break;
+                    }
+                }
+
+                using (var connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    if (options != null && options.UseBatch)
+                    {
+                        int batchSize = options.BatchSize > 0 ? options.BatchSize : options.BacthSizeDefault;
+                        long rows = 0;
+                        string sql = $"COPY {tableName} ({columns}) FROM STDIN (FORMAT BINARY)";
+                        NpgsqlBinaryImporter writer = connection.BeginBinaryImport(sql);
+                        try
+                        {
+                            while (dtReader.Read())
+                            {
+                                writer.StartRow();
+                                for (int i = 0; i < dtReader.FieldCount; i++)
+                                {
+                                    writer.Write(dtReader[i]);
+                                }
+                                rows++;
+                                if (rows % batchSize == 0)
+                                {
+                                    writer.Complete();
+                                    writer.Dispose();
+                                    writer = connection.BeginBinaryImport(sql);
+                                }
+                            }
+                            writer.Complete();
+                        }
+                        catch (System.Exception ex)
+                        {
+                            writer.Dispose();
+                            throw ex;
+                        }
+                        finally
+                        {
+                            writer.Dispose();
+                            rowsCopied = rows;
+                        }
+                    }
+                    else
+                    {
+                        using (var writer = connection.BeginBinaryImport($"COPY {tableName} ({columns}) FROM STDIN (FORMAT BINARY)"))
+                        {
+                            while (dtReader.Read())
+                            {
+                                writer.StartRow();
+                                for (int i = 0; i < dtReader.FieldCount; i++)
+                                {
+                                    writer.Write(dtReader[i]);
+                                }
+                            }
+                            writer.Complete();
+                        }
+                        rowsCopied = dtReader.RecordsAffected;
+                    }
+                }
+                return new SDA_BullkCopyResult() { Success = true, RowsCopied = rowsCopied, Duration = DateTime.Now - startTime };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error during bulk load from DataReader");
+                return new SDA_BullkCopyResult() { Success = false, ErrorMessage = ex.Message };
+            }
+        }
+
+
+        public SDA_DataReaderWrapper BeginDataReader(string query, object parameter)
+        {
+            string connectionString = GetConnectionString();
+            var connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            IDataReader reader = connection.ExecuteReader(query, parameter);
+            Guid guid = Guid.NewGuid();
+            AddOpenConnection(guid, connection);
+            var wrapper = new SDA_DataReaderWrapper(reader, guid);
+            return wrapper;
+        }
+
+        public void EndDataReader(SDA_DataReaderWrapper wrapper)
+        {
+            wrapper.Reader.Close();
+            wrapper.Reader.Dispose();
+            CloseAndRemoveOpenConnection(wrapper.Guid);
+        }
+
+        private void AddOpenConnection(Guid guid, IDbConnection connection)
+        {
+            _openConnections.Add(guid, connection);
+        }
+
+        private void CloseAndRemoveOpenConnection(Guid guid)
+        {
+            IDbConnection connection = null;
+            if (!_openConnections.TryGetValue(guid, out connection))
+            {
+                _logger.LogWarning($"Connection with guid {guid} not found");
+                return;
+            }
+
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+            _openConnections.Remove(guid);
+            connection.Dispose();
         }
 
         public void BulkLoadFromDataTableWithoutMap(string tableName, DataTable dtTable)
--- SDA_SqlServer/Providers/SDA_DBEndpoint_SQLServerCommandProvider.cs
+++ SDA_SqlServer/Providers/SDA_DBEndpoint_SQLServerCommandProvider.cs
@@ -3,16 +3,43 @@ using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using SIPS.Framework.Core.AutoRegister.Interfaces;
+using SIPS.Framework.SDA.Api;
 using SIPS.Framework.SDA.interfaces;
 using SIPS.Framework.SDA.Providers.Base;
+using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 
 namespace SIPS.Framework.SDA_SqlServer.Providers
 {
     public class SDA_DBEndpoint_SQLServerCommandProvider : SDA_BaseProvider, IFCAutoRegisterTransientNamed, ISDA_Endpoint_DBCommandProvider
     {
+        #region Dispose
+        override protected void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+
+                    disposedValue = true;
+                }
+            }
+        }
+
+
+        public new void Dispose()
+        {
+            // Non modificare questo codice. Inserire il codice di pulizia nel metodo 'Dispose(bool disposing)'
+            Dispose(disposing: true);
+            System.GC.SuppressFinalize(this);
+        }
+
+        #endregion
         private readonly ILogger<SDA_DBEndpoint_SQLServerCommandProvider> _logger;
+        private readonly Dictionary<Guid, IDbConnection> _openConnections;
         private readonly IConfiguration _configuration;
         private bool disposedValue;
 
@@ -22,6 +49,7 @@ namespace SIPS.Framework.SDA_SqlServer.Providers
         {
             _logger = logger;
             _configuration = configuration;
+            _openConnections = new Dictionary<Guid, IDbConnection>();
         }
 
         public string ConnectionStringName { get; set; }
@@ -79,6 +107,152 @@ namespace SIPS.Framework.SDA_SqlServer.Providers
             }
         }
 
+
+
+        public SDA_BullkCopyResult BulkLoadFromDataTable(string tableName, DataTable dtTable, SDA_ColumnMappingCollections columnMappings, SDA_CommandOptions options)
+        {
+            long rowsCopied = 0;
+            DateTime startTime = DateTime.Now;
+            try
+            {
+                string connectionString = GetConnectionString();
+
+                string columns = string.Join(",", dtTable.Columns.Cast<DataColumn>().Select(c => c.ColumnName));
+
+                // if columnMappings is not null, it means that the columns are mapped
+                if (columnMappings != null && columnMappings.GetMappings().Any())
+                {
+                    SDA_ColumnMappingCollections.ColumnMappingType[] supportedMappingTypes =
+                        new SDA_ColumnMappingCollections.ColumnMappingType[] {
+                            SDA_ColumnMappingCollections.ColumnMappingType.nameToName,
+                            SDA_ColumnMappingCollections.ColumnMappingType.indexToName,
+                        };
+                    if (!supportedMappingTypes.Contains(columnMappings.MappingType))
+                    {
+                        throw new System.Exception($"Mapping type {columnMappings.MappingType} not supported");
+                    }
+                    switch (columnMappings.MappingType)
+                    {
+                        case SDA_ColumnMappingCollections.ColumnMappingType.nameToName:
+                            columns = string.Join(",", dtTable.Columns.Cast<DataColumn>().Select(c => columnMappings.GetMappings().First(m => m.SourceColumn == c.ColumnName).DestinationColumn));
+                            break;
+                        case SDA_ColumnMappingCollections.ColumnMappingType.indexToName:
+                            columns = string.Join(",", dtTable.Columns.Cast<DataColumn>().Select(c => columnMappings.GetMappings().First(m => m.SourceColumnIndex == c.Ordinal).DestinationColumn));
+                            break;
+                    }
+                }
+
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    // Configurazione di SqlBulkCopy
+                    using (SqlBulkCopy bulkCopy = new SqlBulkCopy(connection))
+                    {
+                        if (columnMappings != null && columnMappings.GetMappings().Any())
+                        {
+                            foreach (var mapping in columnMappings.GetMappings())
+                            {
+                                bulkCopy.ColumnMappings.Add(mapping.SourceColumn, mapping.DestinationColumn);
+                            }
+                        }
+
+                        if (options != null && options.UseBatch)
+                        {
+                            int batchSize = options.BatchSize > 0 ? options.BatchSize : options.BacthSizeDefault;
+                            bulkCopy.BatchSize = batchSize;
+                        }
+
+                        bulkCopy.DestinationTableName = tableName; // Nome della tabella di destinazione nel database
+                        bulkCopy.WriteToServer(dtTable);
+                    }
+                }
+
+                rowsCopied = dtTable.Rows.Count;
+                return new SDA_BullkCopyResult() { Success = true, RowsCopied = rowsCopied, Duration = DateTime.Now - startTime };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error during bulk load from DataTable");
+                return new SDA_BullkCopyResult() { Success = false, ErrorMessage = ex.Message };
+            }
+
+        }
+
+        public SDA_BullkCopyResult BulkLoadFromDataReader(string tableName, IDataReader dtReader, SDA_ColumnMappingCollections columnMappings, SDA_CommandOptions options)
+        {
+            long rowsCopied = 0;
+            DateTime startTime = DateTime.Now;
+            try
+            {
+                string connectionString = GetConnectionString();
+
+                // stringa  con i nomi delle colonne utilizzando dtreader, elenco concatenato con virgola
+                List<string> columnNames = new List<string>();
+
+                for (int i = 0; i < dtReader.FieldCount; i++)
+                {
+                    columnNames.Add(dtReader.GetName(i));
+                }
+                string columns = string.Join(",", columnNames);
+
+                // if columnMappings is not null, it means that the columns are mapped
+                if (columnMappings != null && columnMappings.GetMappings().Any())
+                {
+                    SDA_ColumnMappingCollections.ColumnMappingType[] supportedMappingTypes =
+                        new SDA_ColumnMappingCollections.ColumnMappingType[] {
+                            SDA_ColumnMappingCollections.ColumnMappingType.nameToName,
+                            SDA_ColumnMappingCollections.ColumnMappingType.indexToName,
+                        };
+                    if (!supportedMappingTypes.Contains(columnMappings.MappingType))
+                    {
+                        throw new System.Exception($"Mapping type {columnMappings.MappingType} not supported");
+                    }
+                    switch (columnMappings.MappingType)
+                    {
+                        case SDA_ColumnMappingCollections.ColumnMappingType.nameToName:
+                            columns = string.Join(",", columnNames.Select(c => columnMappings.GetMappings().First(m => m.SourceColumn == c).DestinationColumn));
+                            break;
+                        case SDA_ColumnMappingCollections.ColumnMappingType.indexToName:
+                            columns = string.Join(",", columnNames.Select(c => columnMappings.GetMappings().First(m => m.SourceColumnIndex == columnNames.IndexOf(c)).DestinationColumn));
+                            break;
+                    }
+                }
+
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    // Configurazione di SqlBulkCopy
+                    using (SqlBulkCopy bulkCopy = new SqlBulkCopy(connection))
+                    {
+                        if (columnMappings != null && columnMappings.GetMappings().Any())
+                        {
+                            foreach (var mapping in columnMappings.GetMappings())
+                            {
+                                bulkCopy.ColumnMappings.Add(mapping.SourceColumn, mapping.DestinationColumn);
+                            }
+                        }
+
+                        if (options != null && options.UseBatch)
+                        {
+                            int batchSize = options.BatchSize > 0 ? options.BatchSize : options.BacthSizeDefault;
+                            bulkCopy.BatchSize = batchSize;
+                            bulkCopy.EnableStreaming = true;
+                        }
+
+                        bulkCopy.DestinationTableName = tableName; // Nome della tabella di destinazione nel database
+                        bulkCopy.WriteToServer(dtReader);
+                        rowsCopied = dtReader.RecordsAffected;
+                    }
+                }
+                return new SDA_BullkCopyResult() { Success = true, RowsCopied = rowsCopied, Duration = DateTime.Now - startTime };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error during bulk load from DataReader");
+                return new SDA_BullkCopyResult() { Success = false, ErrorMessage = ex.Message };
+            }
+        }
+
         public void BulkLoadFromDataTableWithoutMap(string tableName, DataTable dataTable)
         {
             string connectionString = GetConnectionString();
@@ -94,28 +268,46 @@ namespace SIPS.Framework.SDA_SqlServer.Providers
             }
         }
 
-        #region Dispose
-        override protected void Dispose(bool disposing)
+        public SDA_DataReaderWrapper BeginDataReader(string query, object parameter)
         {
-            base.Dispose(disposing);
-            if (!disposedValue)
+            string connectionString = GetConnectionString();
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+            IDataReader reader = connection.ExecuteReader(query, parameter);
+            Guid guid = Guid.NewGuid();
+            AddOpenConnection(guid, connection);
+            var wrapper = new SDA_DataReaderWrapper(reader, guid);
+            return wrapper;
+        }
+
+        public void EndDataReader(SDA_DataReaderWrapper wrapper)
+        {
+            wrapper.Reader.Close();
+            wrapper.Reader.Dispose();
+            CloseAndRemoveOpenConnection(wrapper.Guid);
+        }
+
+        private void AddOpenConnection(Guid guid, IDbConnection connection)
+        {
+            _openConnections.Add(guid, connection);
+        }
+
+        private void CloseAndRemoveOpenConnection(Guid guid)
+        {
+            IDbConnection connection = null;
+            if (!_openConnections.TryGetValue(guid, out connection))
             {
-                if (disposing)
-                {
-
-                    disposedValue = true;
-                }
+                _logger.LogWarning($"Connection with guid {guid} not found");
+                return;
             }
-        }
 
-
-        public new void Dispose()
-        {
-            // Non modificare questo codice. Inserire il codice di pulizia nel metodo 'Dispose(bool disposing)'
-            Dispose(disposing: true);
-            System.GC.SuppressFinalize(this);
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+            _openConnections.Remove(guid);
+            connection.Dispose();
         }
-        #endregion
     }
 
 }
