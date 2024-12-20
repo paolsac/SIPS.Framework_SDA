8e8acaee14034dcea9af5eb76d79ce92c47099cb
nh1saccapa <paolo.saccani.nh@pirelli.com>
2024-12-20 12:14:04 +0100

    handle direction and size in ToSDA_DataSourceDefinition

--- SDAC/SDAC_Processor/Extensions/SDAC_OperationDefinition_inline_data_source_Extension.cs
+++ SDAC/SDAC_Processor/Extensions/SDAC_OperationDefinition_inline_data_source_Extension.cs
@@ -6,6 +6,7 @@ using SIPS.Framework.SDAC_Processor.Api.SDAC_ETLDefinition;
 using SIPS.Framework.SDAC_Processor.Helpers;
 using System;
 using System.Collections.Generic;
+using System.Data;
 
 namespace SIPS.Framework.SDAC_Processor.Extensions
 {
@@ -55,10 +56,35 @@ namespace SIPS.Framework.SDAC_Processor.Extensions
                 var parameters = new DynamicParameters();
                 foreach (var parameter in def.Query_parameters)
                 {
+                    ParameterDirection? direction;
+
+                    switch (parameter.direction)
+                    {
+                        case "Input":
+                            direction = ParameterDirection.Input;
+                            break;
+                        case "Output":
+                            direction = ParameterDirection.Output;
+                            break;
+                        case "InputOutput":
+                            direction = ParameterDirection.InputOutput;
+                            break;
+                        case "ReturnValue":
+                            direction = ParameterDirection.ReturnValue;
+                            break;
+                        default:
+                            direction = null;
+                            break;
+                    }
+                    
+                    object value = null;
                     if (parameter.value_as_string != null)
-                        parameters.Add(parameter.name, SDAC_ParseHelper.ParseValue(parameter.value_as_string, parameter.data_type), SDAC_ParseHelper.GetDBType(parameter.data_type));
-                    else
-                        parameters.Add(parameter.name, null,  SDAC_ParseHelper.GetDBType(parameter.data_type));
+                        value = SDAC_ParseHelper.ParseValue(parameter.value_as_string, parameter.data_type);
+                    parameters.Add(parameter.name, value: null, dbType: SDAC_ParseHelper.GetDBType(parameter.data_type), direction: direction, size:parameter.size);
+                    //if (parameter.value_as_string != null)
+                    //    parameters.Add(parameter.name, value: SDAC_ParseHelper.ParseValue(parameter.value_as_string, parameter.data_type), dbType: SDAC_ParseHelper.GetDBType(parameter.data_type), direction: direction);
+                    //else
+                    //    parameters.Add(parameter.name, value: null, dbType: SDAC_ParseHelper.GetDBType(parameter.data_type), direction: direction);
                 }
                 dataSource.ParametersGetter = () => parameters;
             }
